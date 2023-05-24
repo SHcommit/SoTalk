c9413dbe6031f99a6dbed9a1e1f23455332ce2b3
ysh <happysh_s2@naver.com>
2023-05-25 06:06:01 +0900

    [Feat] 간단하게 로그인 vm, vc, extension 생성

--- SoTalk/Source/Feature/Authentication/Login/Extension/LoginViewModel+Transform.swift
+++ SoTalk/Source/Feature/Authentication/Login/Extension/LoginViewModel+Transform.swift
@@ -5,4 +5,24 @@
 //  Created by 양승현 on 2023/05/25.
 //
 
-import Foundation
+import Combine
+
+extension LoginViewModel: ViewModelAssociatedType {
+  struct Input {
+    let appear: AnyPublisher<Void, ErrorType>
+    let viewLoad: AnyPublisher<Void, ErrorType>
+  }
+  
+  enum ErrorType: Error {
+    case none
+    case unexpectedError
+  }
+  
+  enum State {
+    case appear
+    case viewLoad
+    case none
+  }
+  
+  typealias Output = AnyPublisher<State, ErrorType>
+}
--- SoTalk/Source/Feature/Authentication/Login/ViewController/LoginViewController.swift
+++ SoTalk/Source/Feature/Authentication/Login/ViewController/LoginViewController.swift
@@ -6,13 +6,72 @@
 //
 
 import UIKit
+import Combine
 
-class ViewController: UIViewController {
-
+class LoginViewController: UIViewController {
+  // MARK: - Properteis
+  let vm = LoginViewModel()
+  let appear = PassthroughSubject<Void, ErrorType>()
+  let viewLoad = PassthroughSubject<Void, ErrorType>()
+  var subscription = Set<AnyCancellable>()
+  
+  // MARK: - Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
+    bind()
+    viewLoad.send()
     view.backgroundColor = .brown
+    
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    appear.send()
   }
 
 }
 
+// MARK: - ViewBindCase
+extension LoginViewController: ViewBindCase {
+  typealias Input = LoginViewModel.Input
+  typealias ErrorType = LoginViewModel.ErrorType
+  typealias State = LoginViewModel.State
+  
+  func bind() {
+    let input = Input(
+      appear: appear.eraseToAnyPublisher(),
+      viewLoad: viewLoad.eraseToAnyPublisher())
+    let output = vm.transform(input)
+    output.sink { [weak self] completion in
+      switch completion {
+      case .finished: break
+      case .failure(let error):
+        self?.handleError(error)
+      }
+    } receiveValue: { [weak self] in
+      self?.render($0)
+    }.store(in: &subscription)
+
+  }
+  
+  func render(_ state: State) {
+    switch state {
+    case .none:
+      print("none")
+    case .appear:
+      print("appear")
+    case .viewLoad:
+      print("viewLoaded")
+    }
+  }
+  
+  func handleError(_ error: LoginViewModel.ErrorType) {
+    switch error {
+    case .none:
+      print("none")
+    case .unexpectedError:
+      print("unexpectedError")
+    }
+  }
+  
+}
